9585000402983142dabb75a113dc28fba75b5080
vmoreno86 <victor.love86@gmail.com>
2022-09-22 01:12:07 +0200

    Strings con template

---
+++ src/es6/02-strings.js
@@ -0,0 +1,19 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+//Template literals comillas francesas
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//multil-ine strings
+let lorem = 'esto es un string \n ' + 'esto es otra linea';
+let lorem2 = `Esta es una frase epica
+la continuacion de esa frase epica.
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+
